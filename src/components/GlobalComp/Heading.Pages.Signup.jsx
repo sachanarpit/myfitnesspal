8f9a844956bd819a50ef6ffabf8a56565bef3f8f
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-15 02:00:12 +0530

    h1 and h2 created

--- src/components/GlobalComp/Heading.Pages.Signup.jsx
+++ src/components/GlobalComp/Heading.Pages.Signup.jsx
@@ -7,3 +7,18 @@ export const Heading6 = styled.h6`
   font-size: 1.7569546120058566vw;
   color: #363636;
 `;
+
+export const Heading1 = styled.h1`
+  font-family: "NeueEB";
+  font-weight: 950;
+  font-size: 4.685212298682284vw;
+  color: #363636;
+`;
+
+export const Heading2 = styled.h2`
+  font-family: "NeueL";
+  font-weight: normal;
+  font-size: 4.099560761346998vw;
+  color: #363636;
+`;
+
