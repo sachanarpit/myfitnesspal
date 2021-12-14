34b70257d5b30975831b85e02a6b0f729f455fc7
Ishan Singh <ishan_nj2_014@masai.school>
2021-12-15 02:18:43 +0530

    making button

--- src/components/Home-Page/Hero/Hero.jsx
+++ src/components/Home-Page/Hero/Hero.jsx
@@ -1,15 +1,17 @@
-import img from "./Utils/Hero.png"
-import styles from "./Hero.module.css"
+import img from "./Utils/Hero.png";
+import styles from "./Hero.module.css";
 function Hero() {
-    return (
-        <div className={styles.main}>
-            <div className={styles.absolute}>
-            Fitness starts with what you eat.
-            </div>
-            <div className={styles.info}></div>
-            <img src={img} alt="njh" className={styles.img} />
-        </div>
-    )
+  return (
+    <div className={styles.main}>
+      <div className={styles.absolute}>Fitness starts with what you eat.</div>
+      <div className={styles.info}>
+        Take control of your goals. Track calories, break down ingredients, and
+        log activities with MyFitnessPal.
+      </div>
+      <div className={styles.button}></div>
+      <img src={img} alt="njh" className={styles.img} />
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
