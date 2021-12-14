33f1b356285da3cd68bf510bc39895299a09f676
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-14 15:57:14 +0530

    styled component added and logo file added

--- src/components/SignUp-Forms/Form1/index.jsx
+++ src/components/SignUp-Forms/Form1/index.jsx
@@ -1,3 +1,20 @@
+import styled from "styled-components";
+
+const NavGrid = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+`;
+
 export const SignUpForm1 = () => {
-  return <div></div>;
+  return (
+    <div>
+      <NavGrid>
+        <div>arpit</div>
+        <div>arpit</div>
+        <div>arpit</div>
+        <div>arpit</div>
+        <div>arpit</div>
+      </NavGrid>
+    </div>
+  );
 };
