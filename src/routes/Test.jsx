c9a0e422ed8d0192b4d723aa96e936cadb4fb95e
Ishan Singh <ishan_nj2_014@masai.school>
2021-12-15 01:22:44 +0530

    hero image added

--- src/routes/Test.jsx
+++ src/routes/Test.jsx
@@ -1,12 +1,14 @@
 import Footer from "../components/Home-Page/Footer/Footer";
+import Hero from "../components/Home-Page/Hero/Hero";
 import NavbarWithoutLogin from "../components/Home-Page/Navbar_withoutlogin/Navbar_withoutlogin";
 import { SignUpForm1 } from "../components/SignUp-Forms/Form1/index";
 
 export const Test = () => {
   return (
     <>
-      <SignUpForm1 />
+      {/* <SignUpForm1 /> */}
       <NavbarWithoutLogin/>
+      <Hero/>
       <Footer/>
     </>
   );
