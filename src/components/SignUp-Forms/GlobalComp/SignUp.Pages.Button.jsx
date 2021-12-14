cb1b9d664e2500935dcf88909f6b8725742042bd
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-15 00:09:59 +0530

    next and countinue button ready

---
+++ src/components/SignUp-Forms/GlobalComp/SignUp.Pages.Button.jsx
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export const SignUpBackButton = styled.button`
+  border: 1px solid #0066ee;
+  box-sizing: border-box;
+  border-radius: 4px;
+  background: #ffffff;
+  font-family: "NeueB";
+  font-size: 1.3177159590043923vw;
+  width: 12.298682284040996vw;
+  height: 6.437768240343348vh;
+  color: #0066ee;
+
+  transition: 0.3s ease-out;
+  &:hover {
+    background-color: #005bd4;
+    border: 1px solid #005bd4;
+    color: white;
+  }
+`;
+export const SignUpNextButton = styled.button`
+  border: 0px solid rgba(0, 102, 238, 1);
+  box-sizing: border-box;
+  border-radius: 4px;
+  background: rgba(0, 102, 238, 1);
+  font-family: "NeueB";
+  font-size: 1.3177159590043923vw;
+  width: 12.298682284040996vw;
+  height: 6.437768240343348vh;
+  color: #ffffff;
+
+  transition: 0.3s ease-out;
+  &:hover {
+    background-color: #005bd4;
+    border: 1px solid #005bd4;
+    color: white;
+  }
+`;
+export const SignUpContinueButton = styled.button`
+  border: 0px solid rgba(0, 102, 238, 1);
+  box-sizing: border-box;
+  border-radius: 4px;
+  background: rgba(0, 102, 238, 1);
+  font-family: "NeueB";
+  font-size: 1.3177159590043923vw;
+  width: 12.298682284040996vw;
+  height: 6.437768240343348vh;
+  color: #ffffff;
+
+  transition: 0.3s ease-out;
+  &:hover {
+    background-color: #005bd4;
+    border: 1px solid #005bd4;
+    color: white;
+  }
+`;
--- src/routes/Test.jsx
+++ src/routes/Test.jsx
@@ -1,9 +1,19 @@
 import { SignUpForm1 } from "../components/SignUp-Forms/Form1/index";
+import {
+  SignUpBackButton,
+  SignUpNextButton,
+  SignUpContinueButton,
+} from "../components/SignUp-Forms/GlobalComp/SignUp.Pages.Button";
 
 export const Test = () => {
   return (
     <>
       <SignUpForm1 />
+      <SignUpBackButton>Back</SignUpBackButton>
+      <SignUpNextButton>Next</SignUpNextButton>
+      <br />
+      <br />
+      <SignUpContinueButton>CONTINUE</SignUpContinueButton>
     </>
   );
 };
