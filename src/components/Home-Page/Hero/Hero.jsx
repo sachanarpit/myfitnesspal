e6bffaf6765baa37b396f9e02ec84f3a8020c815
Ishan Singh <ishan_nj2_014@masai.school>
2021-12-15 02:03:10 +0530

    hero data added

--- src/components/Home-Page/Hero/Hero.jsx
+++ src/components/Home-Page/Hero/Hero.jsx
@@ -1,8 +1,12 @@
-import img from "./Utils/HERO_IMAGE.jpg"
+import img from "./Utils/Hero.png"
 import styles from "./Hero.module.css"
 function Hero() {
     return (
         <div className={styles.main}>
+            <div className={styles.absolute}>
+            Fitness starts with what you eat.
+            </div>
+            <div className={styles.info}></div>
             <img src={img} alt="njh" className={styles.img} />
         </div>
     )
