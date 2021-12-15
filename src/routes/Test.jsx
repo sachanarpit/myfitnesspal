02baa57f99c187806b7dbde251c48b1a4e92f420
sumitjs7979 <sumitjs79797@gmail.com>
2021-12-15 15:41:18 +0530

    change css in form 1

--- src/routes/Test.jsx
+++ src/routes/Test.jsx
@@ -1,9 +1,17 @@
-import { SignUpForm5 } from "../components/SignUp-Forms/Form5/index";
+// import { SignUpForm1 } from "../components/SignUp-Forms/Form1/index";
+// import { SignUpForm2 } from "../components/SignUp-Forms/Form2/index";
+ import { SignUpForm3 } from "../components/SignUp-Forms/Form3/index";
+// import { SignUpForm4 } from "../components/SignUp-Forms/Form4/index";
+// import { SignUpForm5 } from "../components/SignUp-Forms/Form5/index";
 
 export const Test = () => {
   return (
     <>
-      <SignUpForm5 />
+      {/* <SignUpForm1 /> */}
+      {/* <SignUpForm2 /> */}
+      <SignUpForm3 />
+      {/* <SignUpForm4 />
+      <SignUpForm5 /> */}
     </>
   );
 };
