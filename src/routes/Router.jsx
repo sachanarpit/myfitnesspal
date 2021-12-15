870912229bbc379c077149d96d1ace50f668aa5c
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-16 04:06:43 +0530

    5 signup pages routes are added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,16 +1,32 @@
 import { Route, Switch, Link } from "react-router-dom";
 import { SignUpForm1 } from "../components/SignUp-Forms/Form1";
+import { SignUpForm2 } from "../components/SignUp-Forms/Form2";
+import { SignUpForm3 } from "../components/SignUp-Forms/Form3";
+import { SignUpForm4 } from "../components/SignUp-Forms/Form4";
+import { SignUpForm5 } from "../components/SignUp-Forms/Form5";
 
 export const Routes = () => {
   return (
     <>
       <Switch>
         <Route exact path="/">
-          <Link to="/signup">SignUp 1</Link>
+          <Link to="/signup-1">SignUp 1</Link>
         </Route>
 
-        <Route exact path="/signup">
+        <Route exact path="/signup-1">
           <SignUpForm1 />
+        </Route>
+        <Route exact path="/signup-2">
+          <SignUpForm2 />
+        </Route>
+        <Route exact path="/signup-3">
+          <SignUpForm3 />
+        </Route>
+        <Route exact path="/signup-4">
+          <SignUpForm4 />
+        </Route>
+        <Route exact path="/signup-5">
+          <SignUpForm5 />
         </Route>
 
         <Route path="/products/:id">"new product"</Route>
