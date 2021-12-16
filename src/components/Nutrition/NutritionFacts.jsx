da2a8d2667193708d83990063d12a5a328fc4163
Ishan Singh <ishan_nj2_014@masai.school>
2021-12-17 00:17:32 +0530

    done

--- src/components/Nutrition/NutritionFacts.jsx
+++ src/components/Nutrition/NutritionFacts.jsx
@@ -49,7 +49,46 @@ function NutritionFacts() {
           <div className={styles.light}>35 g</div>
         </div>
       </div>
-      <div className={styles.second}></div>
+      <div className={styles.second}>
+        <div className={styles.sdark}>
+          Submitted on :<span className={styles.date}>11/12/2000</span>{" "}
+        </div>
+        <div className={styles.sdark}>
+          Confirmed Users: <span className={styles.date}>0</span>
+        </div>
+        <div className={styles.fat1}>
+          <div className={styles.dark}>Calcium</div>
+          <div className={styles.light}>0 %</div>
+        </div>
+        <div className={styles.fat2}>
+          <div className={styles.dark}>Iron</div>
+          <div className={styles.light}>0 %</div>
+        </div>
+        <div className={styles.fat2}>
+          <div className={styles.dark}>Vitamin C</div>
+          <div className={styles.light}>0 %</div>
+        </div>
+        <div className={styles.fat2}>
+          <div className={styles.dark}>Vitamin A</div>
+          <div className={styles.light}>0 %</div>
+        </div>
+        <div className={styles.fat2}>
+          <div className={styles.dark}>Sodium</div>
+          <div className={styles.light}>4 mg</div>
+        </div>
+        <div className={styles.fat2}>
+          <div className={styles.dark}>Sugars</div>
+          <div className={styles.light}>0 g</div>
+        </div>
+        <div className={styles.fat2}>
+          <div className={styles.dark}>Protein</div>
+          <div className={styles.light}>3 g</div>
+        </div>
+        <div className={styles.fat2}>
+          <div className={styles.dark}>Pottasium</div>
+          <div className={styles.light}>33 mg</div>
+        </div>
+      </div>
       <div className={styles.third}></div>
     </div>
   );
