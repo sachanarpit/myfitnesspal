dbd3282953c56b4ffed2b6e6a71021e4109783c1
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-17 02:45:38 +0530

    congratulation card is ready

--- src/components/SignUp-Forms/Form9/Form9card.jsx
+++ src/components/SignUp-Forms/Form9/Form9card.jsx
@@ -1,50 +1,59 @@
+import {
+  Body1,
+  Body2,
+  FooterText,
+  Heading4,
+  Heading6,
+} from "../../GlobalComp/Heading.Pages.Signup";
 
+import { ProgressBar8 } from "../../GlobalComp/ProgressBar.Pages";
+import { SignUpContinueButton } from "../../GlobalComp/SignUp.Pages.Button";
+import styles from "./Form9.module.css";
 
-import { Body1, Body2, Heading4, Heading6 } from "../../GlobalComp/Heading.Pages.Signup"
-
-import { ProgressBar8 } from "../../GlobalComp/ProgressBar.Pages"
-import { SignUpContinueButton } from "../../GlobalComp/SignUp.Pages.Button"
-import styles from "./Form9.module.css"
-
-export const Form9card=()=>{
-return (
+export const Form9card = () => {
+  return (
     <>
-    <div className={styles.card9main}>
+      <div className={styles.card9main}>
+        <ProgressBar8></ProgressBar8>
 
-    <ProgressBar8></ProgressBar8>
-    
-    
-    <div className={styles.ques}>
-        <Heading4 >Congratulations</Heading4>
-    </div>
-    <div className={styles.body}>
-    <Body1>Your daily net calorie goal is</Body1>
-    </div>
-    <div className={styles.ques2}>
-        <Heading4 >2,740</Heading4>
-    </div>
-    <div className={styles.sticker}>
-        Calories
-    </div>
-<div className={styles.info}>
-    <Body2>With this plan,you should :</Body2>
-    <div>
-    <span className={styles.gain}>Gain 6 Kg</span><span> By </span><span className={styles.gain}>February 8</span>
-    </div>
-    <hr className={styles.line}/>
-</div>
-<div className={styles.check}>
-        <input className={styles.input} type="checkbox" />
-        <span className={styles.span}>Sign Up for Emails</span>
-    </div>
- <div className={styles.body}>
-     <p>Get recipes, workout, and nutrition & fitness tips from MyFitnessPal <br/>experts. Plus, a first look at new features!</p>
- </div>
- <div className={styles.cont}>
-     <SignUpContinueButton>EXPLORE MYFITNESSPAL</SignUpContinueButton>
- </div>
-    </div>
+        <center>
+          <div className={styles.ques}>
+            <Heading4>Congratulations</Heading4>
+          </div>
+          <div>
+            <Body1>Your daily net calorie goal is</Body1>
+          </div>
+          <div>
+            <Heading4>2,740</Heading4>
+          </div>
+        </center>
+        <div className={styles.sticker}>Calories</div>
+        <div className={styles.info}>
+          <Body2>With this plan,you should :</Body2>
+          <div>
+            <Heading6 style={{ fontFamily: "NeueSB" }}>
+              Gain 6 Kg{" "}
+              <span style={{ fontFamily: "NeueT", fontSize: "1.1vw" }}>By</span>{" "}
+              February 8
+            </Heading6>
+          </div>
+          <hr className={styles.line} />
+        </div>
+        <div className={styles.check}>
+          <input className={styles.input} type="checkbox" />
+          <span className={styles.span}>Sign Up for Emails</span>
+        </div>
+        <div>
+          <FooterText style={{ marginLeft: "5.5vw" }}>
+            Get recipes, workout, and nutrition & fitness tips from MyFitnessPal{" "}
+            <br />
+            experts. Plus, a first look at new features!
+          </FooterText>
+        </div>
+        <div className={styles.cont}>
+          <SignUpContinueButton>EXPLORE MYFITNESSPAL</SignUpContinueButton>
+        </div>
+      </div>
     </>
-)
-
-}
+  );
+};
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,6 +5,7 @@ import { SignUpForm2 } from "../components/SignUp-Forms/Form2";
 import { SignUpForm3 } from "../components/SignUp-Forms/Form3";
 import { SignUpForm4 } from "../components/SignUp-Forms/Form4";
 import { SignUpForm5 } from "../components/SignUp-Forms/Form5";
+import { SignUpForm9 } from "../components/SignUp-Forms/Form9";
 
 export const Routes = () => {
   return (
@@ -26,7 +27,10 @@ export const Routes = () => {
           <SignUpForm4 next={"/signup-5"} prev={"/signup-3"} />
         </Route>
         <Route exact path="/signup-5">
-          <SignUpForm5 next={"/signup-1"} prev={"/signup-4"} />
+          <SignUpForm5 next={"/signup-9"} prev={"/signup-4"} />
+        </Route>
+        <Route exact path="/signup-9">
+          <SignUpForm9 next={"/signup-1"} prev={"/signup-4"} />
         </Route>
         <Route path="/products/:id">"new product"</Route>
         <Route>
