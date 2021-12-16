ccc14c8b2255fa549cdc1c988ffa3c4f338f0286
Ishan Singh <ishan_nj2_014@masai.school>
2021-12-16 19:19:00 +0530

    done

--- src/components/Nutrition/NutritionFacts.jsx
+++ src/components/Nutrition/NutritionFacts.jsx
@@ -2,8 +2,11 @@ import styles from "./NutritionFacts.module.css";
 function NutritionFacts() {
   return (
     <div className={styles.main}>
-      <div className={styles.first}></div>
+      <div className={styles.first}>
+        <div className={styles.vdark}>Nutrition Facts</div>
+      </div>
       <div className={styles.second}></div>
+      <div className={styles.third}></div>
     </div>
   );
 }
