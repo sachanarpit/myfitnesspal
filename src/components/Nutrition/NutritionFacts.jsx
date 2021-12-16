e1032f4e68e147184fd8ee147964487659770322
Ishan Singh <ishan_nj2_014@masai.school>
2021-12-16 19:56:40 +0530

    done

--- src/components/Nutrition/NutritionFacts.jsx
+++ src/components/Nutrition/NutritionFacts.jsx
@@ -1,9 +1,16 @@
 import styles from "./NutritionFacts.module.css";
+import vector from "./Utils/Vector.svg";
 function NutritionFacts() {
   return (
     <div className={styles.main}>
       <div className={styles.first}>
         <div className={styles.vdark}>Nutrition Facts</div>
+        <div className={styles.poha}>
+          <span className={styles.Pl}>Poha</span>
+          <span>
+            <img src={vector} alt="mn" />
+          </span>
+        </div>
       </div>
       <div className={styles.second}></div>
       <div className={styles.third}></div>
