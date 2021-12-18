f47dca04fa2923f541280732a8c8c60c90a50ed9
Ishan Singh <ishan_nj2_014@masai.school>
2021-12-19 00:15:54 +0530

    home page done

--- src/components/Home-Page/Index.jsx
+++ src/components/Home-Page/Index.jsx
@@ -2,10 +2,11 @@ import { LoaderComp } from "../GlobalComp/Loading.Pages";
 import Connect from "./Connect/Connect";
 import Footer from "./Footer/Footer";
 import Hero from "./Hero/Hero";
-import Logobar from "./Logobar/Logobar"
+import Logobar from "./Logobar/Logobar";
 import NavbarWithoutLogin from "./Navbar_withoutlogin/Navbar_withoutlogin";
 import { useEffect } from "react";
 import { useState } from "react";
+import Recepie from "../Recepie/Recepie";
 
 function IndexHome({ next }) {
   const [spinner, setSpinner] = useState(true);
@@ -17,9 +18,10 @@ function IndexHome({ next }) {
     <LoaderComp />
   ) : (
     <div>
-    <Logobar/>
+      <Logobar />
       <NavbarWithoutLogin />
       <Hero next={next} />
+      <Recepie />
       <Connect />
       <Footer />
     </div>
