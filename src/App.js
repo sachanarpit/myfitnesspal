75175585d610434d3137de949855ad3b58cbbdf3
Ishan Singh <ishan_nj2_014@masai.school>
2021-12-19 19:50:17 +0530

    done

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import TestArpit from "./routes/Test.arpit";
 function App() {
   return (
     <div className="App">
-      {/* <Routes /> */}
-      <Test />
+      <Routes />
+      {/* <Test /> */}
 
       {/* <Test /> */}
 
       {/* <LoaderComp /> */}
-      <TestArpit />
+      {/* <TestArpit /> */}
     </div>
   );
 }
