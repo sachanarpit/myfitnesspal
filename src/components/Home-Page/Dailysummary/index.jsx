c098a3a9d905b94ea6cc4cfaedf028ed58a459c6
sumitjs7979 <sumitjs79797@gmail.com>
2021-12-19 16:27:48 +0530

    logo bar css change

--- src/components/Home-Page/Dailysummary/index.jsx
+++ src/components/Home-Page/Dailysummary/index.jsx
@@ -1,7 +1,7 @@
 
 
 
-import { Form2Navbar } from "./Navbar.Form2";
+import Logobar from "./Logobar";
 import { NavbarWithhome } from "./Navbar_withhome";
 import { NavbarWithhome1 } from "./Navbar_withhome1";
 import { Summary } from "./Summary";
@@ -10,9 +10,10 @@ import { Summary } from "./Summary";
 export const Dailypage = ({next,prev}) => {
   const calorie=3600;
   const email="Avishek_uxd2_092@masai.school";
+  const name="Avishek_nayak";
   return (
     <div>
-        <Form2Navbar />
+        <Logobar name={name} />
         <NavbarWithhome />
         <NavbarWithhome1 />
      <Summary calorie={calorie} next={next} prev={prev} email={email}/>
