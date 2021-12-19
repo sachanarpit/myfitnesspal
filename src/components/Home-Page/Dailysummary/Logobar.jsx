f6148b1fc9d18978da4327aa823b237598d1f02a
Arpit Sachan <sachanarpitknp@gmail.com>
2021-12-19 23:37:27 +0530

    logo added link

--- src/components/Home-Page/Dailysummary/Logobar.jsx
+++ src/components/Home-Page/Dailysummary/Logobar.jsx
@@ -1,27 +1,27 @@
+import { Link } from "react-router-dom";
 import styles from "./Logobar.module.css";
-function Logobar({name}) {
+function Logobar({ name }) {
   return (
     <div className={styles.main}>
       <div className={styles.img}>
-        <img src="logo.svg" alt="logo" />
+        <Link to="/user-home">
+          <img src="logo.svg" alt="logo" />
+        </Link>
       </div>
 
       <div className={styles.log1}>
-      <div className={styles.login1}>{name}</div>
+        <div className={styles.login1}>{name}</div>
         <div className={styles.vertical1}></div>
         <div className={styles.login1}>
-            <img src="Gmail.svg" />
+          <img src="Gmail.svg" />
         </div>
         <div className={styles.vertical1}></div>
       </div>
       <div className={styles.log}>
-      
         <div className={styles.login}>Help</div>
         <div className={styles.vertical}></div>
         <div className={styles.login}>Setting</div>
-        
       </div>
-    
     </div>
   );
 }
